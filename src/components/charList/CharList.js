f4b80058052389ada55ebade2f71aa277aac480f
DESKTOP-7CB8L9L\ASUS <alex.svyridov@vitalerter.com>
2022-05-03 16:38:54 +0300

    Creater random character.

---
+++ src/components/charList/CharList.js
@@ -0,0 +1,56 @@
+import './charList.scss';
+import { Component } from 'react';
+import abyss from '../../resources/img/abyss.jpg';
+import MarvelService from '../../services/MarvelService'
+
+class CharList extends Component {
+
+    state = {
+        chars: [],
+        loading: true,
+    }
+
+    onCharLoaded = (chars) => {
+        this.setState({ 
+            chars,
+            loading: false,
+        })
+    }
+    marvelService = new MarvelService();
+        updateChar = () => {
+        this.marvelService
+            .getAllCharacters()
+            .then(this.onCharLoaded)
+    }
+
+    componentDidMount(){
+        this.updateChar()
+    }
+
+
+    render(){
+        const {chars} = this.state
+        const elements = chars.map(item => {
+            const {name, thumbnail} = item
+            return (
+                <li className="char__item">
+                    <img src={thumbnail} alt="abyss"/>
+                    <div className="char__name">{name}</div>
+                </li>
+            )
+        })
+        return (
+                <div className="char__list">
+                    <ul className="char__grid">
+                        {elements}
+                    </ul>
+                    <button className="button button__main button__long">
+                        <div className="inner">load more</div>
+                    </button>
+                </div>
+            )
+    }
+    
+}
+
+export default CharList;
